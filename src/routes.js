50baf7aa74df4d850255d2edd61532335340ac5c
Igor Yuzkiv <igor97w@gmail.com>
2024-12-24 18:16:31 +0200

    navigation

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,21 @@ export const router = createRouter(document.querySelector('#app .route-view'), [
     {
         name: 'home',
         path: '/',
-        component: '/src/pages/home.page.js',
+        component: '/my-cv/src/pages/home.page.js',
     },
     {
         name: 'vue-certificate',
         path: '/vue-certificate',
-        component: '/src/pages/vue-certificate.page.js',
+        component: '/my-cv/src/pages/vue-certificate.page.js',
     },
     {
         name: 'experience',
         path: '/experience',
-        component: '/src/pages/experience.page.js',
+        component: '/my-cv/src/pages/experience.page.js',
     },
     {
         name: 'todo',
         path: '/todo',
-        component: '/src/pages/todo.page.js',
+        component: '/my-cv/src/pages/todo.page.js',
     },
 ])
